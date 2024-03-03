1d2afb6e2a58580780d35beb50d47633d20d72a7
dibkb <dibas9110@gmail.com>
2024-03-04 00:29:09 +0530

    node server

--- web/index.js
+++ web/index.js
@@ -1,1 +1,13 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = fs.promises;
 
+const PORT = process.env.PORT || 4000;
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+});
+
+server.listen(PORT, () => {
+  console.log("server runnning...");
+});
